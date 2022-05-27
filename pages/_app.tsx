f34fb169abc1aae73bf50f45ad3c4bcd46559032
Salvador Emmanuel Juarez Granados <sjuarez@arkusnexus:wq.com>
2022-05-27 17:33:38 -0500

    :rocket: :lipstick: feature/theme config Mui :skull:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-
+import { ThemeProvider } from "@mui/material/styles"
+import { nordenPurple } from "../theme.config"
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={nordenPurple}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
